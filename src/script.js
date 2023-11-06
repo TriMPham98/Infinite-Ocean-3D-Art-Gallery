372f687b9b0d0c8c273468ff7172429505cb7abe
Tri <trimpham98@gmail.com>
2023-11-05 18:55:08 -0800

    Remove sphere, cube, and torus

--- src/script.js
+++ src/script.js
@@ -34,22 +34,13 @@ const material = new THREE.MeshStandardMaterial();
 material.roughness = 0.4;
 
 // Objects
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), material);
-sphere.position.x = -1.5;
 
-const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material);
-
-const torus = new THREE.Mesh(
-  new THREE.TorusGeometry(0.3, 0.2, 32, 64),
-  material
-);
-torus.position.x = 1.5;
 
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
 plane.rotation.x = -Math.PI * 0.5;
 plane.position.y = -0.65;
 
-scene.add(sphere, cube, torus, plane);
+scene.add(plane);
 
 /**
  * Sizes
@@ -110,13 +101,6 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  sphere.rotation.y = 0.1 * elapsedTime;
-  cube.rotation.y = 0.1 * elapsedTime;
-  torus.rotation.y = 0.1 * elapsedTime;
-
-  sphere.rotation.x = 0.15 * elapsedTime;
-  cube.rotation.x = 0.15 * elapsedTime;
-  torus.rotation.x = 0.15 * elapsedTime;
 
   // Update controls
   controls.update();
