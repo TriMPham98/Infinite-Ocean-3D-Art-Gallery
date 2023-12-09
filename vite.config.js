b2ea64dba5595f9c6a273c058094b89754f703ac
Tri <trimpham98@gmail.com>
2023-12-08 23:26:07 -0800

    Change path to include the root

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./assets", // Set the base to the public path, adjust if necessary
+  base: "./", // Adjust this path if your app is not served from the root
   build: {
     assetsDir: "assets", // Keep assets within an assets directory
     rollupOptions: {
-      // Configure Rollup options if necessary, for more control over the output structure
       output: {
         chunkFileNames: "assets/[name]-[hash].js",
         entryFileNames: "assets/[name]-[hash].js",
@@ -14,14 +13,11 @@ export default defineConfig({
       },
     },
   },
-  // Include other configurations like plugins if needed
-  // Add alias if you have specific rewriting rules for assets
   resolve: {
     alias: {
       // Add aliases if necessary
     },
   },
-  // Include all your assets to the build
   assetsInclude: [
     "**/*.jpg",
     "**/*.png",
@@ -31,5 +27,4 @@ export default defineConfig({
     "**/*.woff",
     "**/*.woff2",
   ],
-  // Ensure to test this configuration locally by running `vite build` before deploying
 });
