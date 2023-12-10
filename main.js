bc01fadc5fde4d8ef31bf046ee4cf60d4b4a098c
Tri <trimpham98@gmail.com>
2023-12-09 16:16:41 -0800

    Remove TODO comment

--- main.js
+++ main.js
@@ -362,7 +362,6 @@ function onCanvasHover(event) {
 // TODO: Make canvas frame rectLight cooler when night mode is toggled
 // TODO: Add stars when night mode is toggled
 // TODO: Experiment with easing options for gsap
-// TODO: Align sun azimuth with image2.jpg and move sunObject
 // TODO: Add function to zoom in on current canvas on double click
 
 function toggleNightMode() {
